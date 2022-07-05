0eaba276ae9f982beedd4ec0cb5b0383d808255f
shamisashams <shamisaashams@gmail.com>
2022-07-05 11:28:19 +0400

    it didnt work , maybe cuz of npm run watch

--- public/js/resources_js_Layouts_AppEng_css.js
+++ public/js/resources_js_Layouts_AppEng_css.js
@@ -17,7 +17,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_0___default()(function(i){return i[1]});
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\n    font-family: \"brushsci\";\n    src: url(/assets/fonts/BRUSHSCI.ttf);\n}\n.cursive {\n    font-family: \"brushsci\"!important;\n}\n", ""]);
+___CSS_LOADER_EXPORT___.push([module.id, "@font-face {\r\n    font-family: \"brushsci\";\r\n    src: url(/assets/fonts/BRUSHSCI.ttf);\r\n}\r\n.cursive {\r\n    font-family: \"brushsci\"!important;\r\n}\r\n", ""]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
